4d02b5ef58ff650a9ab98836e4559e2d6d19e273
Ana Castellanos <lia150687@gmail.com>
2022-09-28 12:30:54 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/demomlv1/DemoMLv1Application.kt
@@ -0,0 +1,11 @@
+package com.example.demomlv1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoMLv1Application
+
+fun main(args: Array<String>) {
+	runApplication<DemoMLv1Application>(*args)
+}
---
+++ src/test/kotlin/com/example/demomlv1/DemoMLv1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.demomlv1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoMLv1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
